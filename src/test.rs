5318978a57cf84fdbc0cabefa7a6198958820e26
Felix Ableitner <me@nutomic.com>
2022-07-11 16:27:21 +0200

    Fix tests

--- src/test.rs
+++ src/test.rs
@@ -1,11 +1,17 @@
 #[cfg(test)]
 mod tests {
     use crate::init_rocket;
+    use log::LevelFilter;
     use rocket::{http::uri::Origin, local::blocking::Client};
     use std::env;
 
-    fn test_with_uri(uri: Origin) {
+    #[ctor::ctor]
+    fn init() {
         env::set_var("LEMMY_INTERNAL_HOST", "https://lemmy.ml");
+        env_logger::builder().filter_level(LevelFilter::Warn).init();
+    }
+
+    fn test_with_uri(uri: Origin) {
         let rocket = init_rocket();
         let client = Client::tracked(rocket).unwrap();
         let res = client.get(uri).dispatch();
